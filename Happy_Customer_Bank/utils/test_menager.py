6cab9fcb536255d9056d73d8f15c5e6c930f23d6
Katarzyna Koloda <koloda.katarzyna@gmail.com>
2023-10-04 20:22:57 +0000

    added category encoders tests

--- Happy_Customer_Bank/utils/test_menager.py
+++ Happy_Customer_Bank/utils/test_menager.py
@@ -15,74 +15,120 @@ params_to_save = ["class_weight", "min_samples_leaf", "max_depth", "n_estimators
 missings_handling_models = ["DecisionTreeClassifier", "BaggingClassifier", "HistGradientBoostingClassifier"]
 
 
-def scores(pipeline, X, y, model_name, model_params, imputation):
+def collect_tests_results(models, X, y, test="imputation", preprocessors=None):
 
+    results = []
+
+    num_features = X.select_dtypes(float, int).columns
+    cat_features = X.select_dtypes(object).columns
+
+    for model in models:
+
+        model_name = model.__class__.__name__
+        model_params = ", ".join([f"{param}: {getattr(model, param)}"
+                                  for param in params_to_save
+                                  if hasattr(model, param)])
+
+        if preprocessors is None and test == "imputation":
+            if model_name in missings_handling_models:
+
+                preprocessor = ColumnTransformer([
+                    ("categorical", 
+					 OneHotEncoder(), 
+					 cat_features)],
+												 remainder="passthrough")
+
+                result = scores(preprocessor, 
+								model, 
+								X, y, 
+								model_name, 
+								model_params, 
+								test="imputation", 
+								imputation="none")
+
+                results.append(result)
+
+        if preprocessors is not None and test == "imputation":
+            for imputer_name, preprocessor in preprocessors:
+
+                preprocessor = ColumnTransformer([
+                    ("numerical", 
+					 preprocessor, 
+					 num_features),
+                    ("categorical", 
+					 OneHotEncoder(), 
+					 cat_features)],
+												 remainder="passthrough")
+
+                result = scores(preprocessor,
+								model, 
+								X, y, 
+								model_name, 
+								model_params, 
+								test="imputation", 
+								imputation=imputer_name)
+
+                results.append(result)
+
+        if preprocessors is not None and test == "encoders":
+            for preprocessor in preprocessors:
+
+                preprocessor = ColumnTransformer([
+                    ("numerical", 
+					 SimpleImputer(strategy="constant", 
+												fill_value=-1, 
+												add_indicator = True), 
+					 num_features),
+                    ("categorical", 
+					 preprocessor, 
+					 cat_features)],
+												 remainder="passthrough")
+
+                encoder = preprocessor.transformers[1][1].__class__.__name__
+
+                result = scores(preprocessor, 
+								model, 
+								X, y, 
+								model_name, 
+								model_params, 
+								test="encoders", 
+								encoder=encoder)
+
+                results.append(result)
+
+    return results
+
+
+def scores(preprocessor, model, X, y, model_name, model_params, test, imputation=None, encoder=None):
+	
+    pipeline = make_pipeline(preprocessor, model)
     cv_scheme = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
     
-    cv_results = cross_validate(pipeline,
-                        X, y,
-                        scoring="roc_auc",
-                        cv=cv_scheme,
-                        n_jobs=-1)
+    cv_results = cross_validate(pipeline, 
+								X, y, 
+								scoring="roc_auc", 
+								cv=cv_scheme, 
+								n_jobs=-1)
 
-    return {"Model": model_name,
-            "Parameters": model_params,
-            "ROC_AUC": round(cv_results["test_score"].mean(), 4),
-            "Imputation": imputation,
-            "Time[s]": round(cv_results["fit_time"].mean(), 2)}
+    if test == "imputation":
+        return {"Model": model_name,
+                "Parameters": model_params,
+                "ROC_AUC": round(cv_results["test_score"].mean(), 4),
+                "Imputation": imputation,
+                "Time[s]": round(cv_results["fit_time"].mean(), 2)}
 
-
-def collect_imputation_results(models, X, y, num_features, cat_features, preprocessors):
-
-    results = []
-
-    for model in models:
-        
-        model_name = model.__class__.__name__
-        model_params = ", ".join([f"{param}: {getattr(model, param)}" for param in params_to_save if hasattr(model, param)])
-
-        for imputer_name, preprocessor in preprocessors:
-
-            preprocessor = ColumnTransformer([
-                    ("numerical", preprocessor, num_features),
-                    ("categorical", OneHotEncoder(), cat_features)],
-                    remainder="passthrough")
-            
-            pipeline = make_pipeline(preprocessor, model)
-            
-            imputation = imputer_name
-            result = scores(pipeline, X, y, model_name, model_params, imputation)
-            results.append(result)
-
-    return results
-
-
-def collect_without_imputation_results(models, X, y, cat_features):
-
-    results = []
-
-    for model in models:
-        
-        model_name = model.__class__.__name__
-        model_params = ", ".join([f"{param}: {getattr(model, param)}" for param in params_to_save if hasattr(model, param)])
-
-        if model_name in missings_handling_models:
-        
-            preprocessor = ColumnTransformer([("categorical", OneHotEncoder(), cat_features)],
-                        remainder="passthrough")
-            imputation = "None"
-            pipeline = make_pipeline(preprocessor, model)
-            result = scores(pipeline, X, y, model_name, model_params, imputation)
-            results.append(result)
-
-    return results
+    if test == "encoders":
+        return {"Model": model_name,
+                "Parameters": model_params,
+                "ROC_AUC": round(cv_results["test_score"].mean(), 4),
+                "Encoder": encoder,
+                "Time[s]": round(cv_results["fit_time"].mean(), 2)}
 
 
 def create_results_dataframe(*args):
     
     if len(args) == 1:
-        df = pd.DataFrame(args[0])
-        
+        df = pd.DataFrame(args[0])        
     elif len(args) > 1:
         df = pd.concat([pd.DataFrame(arg) for arg in args])
         
