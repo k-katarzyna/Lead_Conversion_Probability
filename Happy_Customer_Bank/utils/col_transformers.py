138af2f2c7c36c7e58790e681189cf663f9bbbbd
Katarzyna Koloda <koloda.katarzyna@gmail.com>
2023-10-05 19:19:58 +0000

    functionality extension and docstrings added

--- Happy_Customer_Bank/utils/col_transformers.py
+++ Happy_Customer_Bank/utils/col_transformers.py
@@ -1,87 +1,143 @@
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
-from dateutil.relativedelta import relativedelta
 
 
-class ZeroOneCategoryEncoder(BaseEstimator, TransformerMixin):
+class ZeroOneEncoder(BaseEstimator, TransformerMixin):
 
     """
-    Transformer for:
-	- encoding categorical features with many categories and NaN values, when it is assumed that the value may not be significant, but its presence is,
-	- encoding categorical features with exactly 2 unique values.
+    Transformer for encoding categorical features with two unique values or handling NaN values.
 
     This transformer automatically detects columns with NaN values and binary categorical columns.
     For columns with NaN values, it replaces NaN with 0 and non-NaN values with 1.
     For binary categorical columns, it encodes one value as 0 and the other as 1.
-    
+
+    Parameters:
+    -----------
+    None
     """
-    
-    def __init__(self):
-        self.columns_with_nan = []
-        self.binary_columns = []
-    
-    def fit(self, X, y=None):
-        self.columns_with_nan = [col for col in X.columns if X[col].isna().any()]
-        self.binary_columns = [col for col in X.columns if len(X[col].unique()) == 2]
+
+    def fit(self, X, y = None):
+        self.columns_with_nan_ = [col for col in X.columns if X[col].isna().any()]
+        self.binary_columns_ = [col for col in X.columns if X[col].nunique() == 2]
         return self
-    
+
     def transform(self, X):
         transformed = X.copy()
-        
-        for column in self.columns_with_nan:
-            transformed[column] = transformed[column].apply(lambda x: 1 if pd.notna(x) else 0)
-        
-        for column in self.binary_columns:
-            unique_values = transformed[column].unique()
-            transformed[column] = transformed[column].apply(lambda x: 1 if x == unique_values[1] else 0)
+
+        for col in self.columns_with_nan:
+            transformed[col] = transformed[col].apply(
+                lambda x:
+                0 if pd.isna(x) or (str(x).isdigit())
+                else 1
+            )
+
+        for col in self.binary_columns:
+            unique_values = transformed[col].unique()
+            transformed[col] = transformed[col].apply(
+                lambda x:
+                0 if x == unique_values[0]
+                else 1
+            )
 
         return transformed
 
 
-class MultiLevelCategoryEncoder(BaseEstimator, TransformerMixin):
-	
+class MultiTaskEncoder(BaseEstimator, TransformerMixin):
+    
     """
-    Transformer for encoding multilevel categorical features.
+    Transformer for encoding multilevel categorical features and optional rare category grouping.
 
     This transformer detects columns with more than two unique values and encodes them by assigning
-    consecutive numerical values in the range (1, number of categories) based on the frequency of each category. 
+    consecutive numerical values in the range (number of categories, 1) based on the frequency of each category. 
     If NaNs are present, they will be replaced by 0.
 
-    Additionally, it handles binary categorical columns by encoding them as 0 and 1.
+    Additionally, it handles binary categorical columns by encoding them as 2 and 1, wchich is also based on frequency
+    (2 for more numerous category).
+
+    Parameters:
+    -----------
+    group_rare : bool, default=False
+        If True, rare categories are grouped in the multilevel categorical columns. Rare categories are defined as
+        categories with a frequency of less than the specified threshold (rare_threshold).
+    rare_threshold : int, default=1000
+        The threshold below which categories are considered rare when grouping. Categories with a frequency
+        less than this threshold will be grouped into a single "Rare" category.
+    group_only : bool, default=False
+        If used, the 'group_rare' parameter must also be set to true.
+        If True, the transformer will only perform rare category grouping and return the transformed column(s)
+        without additional encoding. If set to False, the column(s) will be both grouped (if group_rare is True)
+        and encoded.
+
+    Attributes:
+    -----------
+    multi_level_columns_ : list
+        A list of column names containing multilevel categorical features.
+    binary_columns_ : list
+        A list of column names containing binary categorical features.
+    rare_categories_ : dict
+        A dictionary containing information about rare categories that were grouped. Keys represent column names,
+        and values are lists of categories that were grouped into the "Others" category for each respective column.
     """
 
-    def __init__(self):
-        self.multi_level_columns = []
-        self.binary_columns = []
+    def __init__(self, 
+                 group_rare = False, 
+                 rare_threshold = 1000, 
+                 group_only = False):
+        
+        self.group_rare = group_rare
+        self.rare_threshold = rare_threshold
+        self.group_only = group_only
 
-    def fit(self, X, y=None):
-        self.multi_level_columns = [col for col in X.columns if X[col].nunique() > 2]
-        self.binary_columns = [col for col in X.columns if X[col].nunique() == 2]
+    def fit(self, X, y = None):
+        self.multi_level_columns_ = [col for col in X.columns if X[col].nunique() > 2]
+        self.binary_columns_ = [col for col in X.columns if X[col].nunique() == 2]
+
+        if self.group_rare:
+            self.rare_categories_ = {}
+			
+            for col in self.multi_level_columns_:
+                category_counts = X[col].value_counts()
+                self.rare_categories_[col] = category_counts[category_counts < self.rare_threshold].index
+
         return self
 
     def transform(self, X):
         transformed = X.copy()
 
-        for column in self.multi_level_columns:
-            category_counts = transformed[column].value_counts()
-            category_mapping = {category: len(category_counts) - i for i, category in enumerate(category_counts.index)}
-            transformed[column] = transformed[column].map(category_mapping)
-            
-            if transformed[column].isna().any():
-                transformed[column] = transformed[column].fillna(0).astype(int)
+        for col in self.multi_level_columns_:
+            if self.group_rare and col in self.rare_categories_:
+                transformed[col] = transformed[col].apply(
+                    lambda x:
+                    "Others" if x in self.rare_categories_[col] 
+					else x
+                )
+                
+                if self.group_only:
+                    return transformed
 
-        for column in self.binary_columns:
-            unique_values = transformed[column].value_counts().index
-            transformed[column] = transformed[column].apply(lambda x: 1 if x == unique_values[0] else 0)
+            category_counts = transformed[col].value_counts()
+            category_mapping = {category: len(category_counts) - i
+                                for i, category in enumerate(category_counts.index)}
+            transformed[col] = transformed[col].map(category_mapping)
+
+            if transformed[col].isna().any():
+                transformed[col] = transformed[col].fillna(0).astype(int)
+
+        for col in self.binary_columns_:
+            unique_values = transformed[col].value_counts().index
+            transformed[col] = transformed[col].apply(
+                lambda x:
+                1 if x == unique_values[0]
+                else 0
+            )
 
         return transformed
-
 
 
 class DateEncoder(BaseEstimator, TransformerMixin):
     
     """
-    A transformer for encoding date columns.
+    Transformer for encoding date columns.
 
     This transformer is designed to work with date columns. 
     It offers three encoding options: age calculation, day of the year extraction, and month extraction.
@@ -96,22 +152,27 @@ class DateEncoder(BaseEstimator, TransformerMixin):
     If extract_from_date is None, the transformer returns pandas datetime object.
     """
     
-    def __init__(self, extract_from_date=None, date_for_calc="04-08-2015"):
+    def __init__(self, 
+				 extract_from_date = None, 
+				 date_for_calc = "04-08-2015"):
+		
         self.extract_from_date = extract_from_date
         self.date_for_calc = date_for_calc
         
-    def fit(self, X, y=None):
+    def fit(self, X, y = None):
         return self
     
     def transform(self, X):
         transformed = X.copy()
         transformed = pd.to_datetime(transformed)
-        transformed = transformed.apply(lambda x: x - pd.DateOffset(years=100) if x.year > 2014 else x)
         
         if self.extract_from_date == "age":
+            transformed = transformed.apply(
+				lambda x: x - pd.DateOffset(years=100) if x.year > 2014 else x
+			)
             reference_date = pd.to_datetime(self.date_for_calc)
-            return relativedelta(reference_date, transformed)
-        
+            return (reference_date - transformed).dt.days // 365
+
         elif self.extract_from_date == "day_of_year":
             return transformed.dt.dayofyear
 			
