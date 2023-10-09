965da78d29ed6db3c75f5ffa083f1e2b265a06e8
Katarzyna Koloda <koloda.katarzyna@gmail.com>
2023-10-09 20:26:52 +0000

    phase one of the tests completed

--- Happy_Customer_Bank/utils/data_prep.py
+++ Happy_Customer_Bank/utils/data_prep.py
@@ -17,16 +17,13 @@ def data_preparing_v1(data):
     """
     data = data.copy()
 
-    data.drop("ID", axis = 1, inplace=True)
+    data.drop(["ID", "Lead_Creation_Date", "Device_Type"], axis = 1, inplace = True)
     data[["Loan_Amount_Applied", "Loan_Tenure_Applied", "Existing_EMI"]] = data[["Loan_Amount_Applied", "Loan_Tenure_Applied", "Existing_EMI"]].fillna(0)
 
     data["City"] = data["City"].apply(
         lambda x: "Y" if x in biggest_cities else "N" if pd.notna(x) else x)
 
     data["DOB"] = data["DOB"].apply(lambda x: int(x[-2:]))
-
-    data["Lead_Creation_Date"] = pd.to_datetime(data["Lead_Creation_Date"])
-    data["Lead_Creation_Date"] = data["Lead_Creation_Date"].apply(lambda x: x.day_of_year)
 
     data["Salary_Account"] = data["Salary_Account"].apply(lambda x: 1 if pd.notna(x) else 0)
     data["Employer_Name"] = data["Employer_Name"].apply(lambda x: 0 if pd.isna(x) or (str(x).isdigit()) else 1)
@@ -36,7 +33,6 @@ def data_preparing_v1(data):
         data[feature].replace(rare_values, "Others", inplace = True)
 
     data.rename(columns={"DOB": "Year_Of_Birth",
-                         "Lead_Creation_Date": "Lead_Creation_Day",
                          "City": "Is_Big_City",
                          "Salary_Account": "Account_Provided",
                          "Employer_Name": "Employer_Provided"},
--- Happy_Customer_Bank/utils/test_menager.py
+++ Happy_Customer_Bank/utils/test_menager.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import numpy as np
-from sklearn.compose import ColumnTransformer, make_column_transformer
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.compose import make_column_transformer
 from sklearn.ensemble import (AdaBoostClassifier, BaggingClassifier,
                              HistGradientBoostingClassifier, RandomForestClassifier)
 from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import SimpleImputer, KNNImputer, IterativeImputer
+from sklearn.impute import SimpleImputer
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import cross_validate, StratifiedKFold
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import OneHotEncoder, TargetEncoder, PowerTransformer, MinMaxScaler
+from sklearn.model_selection import cross_validate, StratifiedKFold, GridSearchCV
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.preprocessing import OneHotEncoder, TargetEncoder
 from sklearn.feature_selection import SelectFromModel
 
+cv_scheme = StratifiedKFold(n_splits = 5, shuffle = True, random_state = 42)
 
-def collect_tests_results(models, X, y, test, preprocessors = None):
+def collect_tests_results(X, y, test, models = None, preprocessors = None, grid_search = None):
     """
     Collects test results for different machine learning models.
 
@@ -21,117 +21,146 @@ def collect_tests_results(models, X, y, test, preprocessors = None):
         models (list): List of models to evaluate.
         X (pd.DataFrame): Input data.
         y (pd.Series): Target labels.
-        test (str): Type of test to perform, either "imputation", "encoders", "feature_selection".
+        test (str): Type of test to perform, either "imputation", "cat_encoding", "feature_selection", "searching_params",.
         preprocessors (list): List of preprocessors.
+        grid_search (tuple): (model, param_grid), only for "searching_params" test.
 
     Returns:
         list: List of dictionaries containing test results.
     """
-    results = []
 
     num_features = X.select_dtypes(float, int).columns
     cat_features = X.select_dtypes(object).columns
 
-    params_to_save = ["class_weight", "min_samples_leaf", "n_estimators"]
-    missings_handling_models = ["DecisionTreeClassifier", "BaggingClassifier", "HistGradientBoostingClassifier"]
-
-    for model in models:
-        model_name = model.__class__.__name__
-        model_params = ", ".join([f"{param}: {getattr(model, param)}"
-                                  for param in params_to_save
-                                  if hasattr(model, param)])
-
-        if test == "imputation" and preprocessors is None:
-            if model_name in missings_handling_models:
-                
-                preprocessor = make_column_transformer((OneHotEncoder(), cat_features),
-                                                       remainder="passthrough")
-                
-                pipeline = make_pipeline(preprocessor, model)
-                
-                result = scores(pipeline,
-                                X, y, 
-                                model_name, 
-                                model_params, 
-                                test = test, 
-                                imputation = "none")
-
-                results.append(result)
-
-        if test == "imputation" and preprocessors is not None:
-            for imputer_name, preprocessor in preprocessors:
-                
-                preprocessor = make_column_transformer((preprocessor, num_features),
-                                                       (OneHotEncoder(), cat_features),
-                                                       remainder = "passthrough")
-
-                pipeline = make_pipeline(preprocessor, model)
-                
-                result = scores(pipeline,
-                                X, y, 
-                                model_name, 
-                                model_params, 
-                                test = test, 
-                                imputation = imputer_name)
-
-                results.append(result)
-
-        if test == "encoders":
-            for preprocessor in preprocessors:
-                
-                preprocessor = make_column_transformer((SimpleImputer(strategy = "constant",
-                                                                      fill_value = -1,
-                                                                      add_indicator = True),
-                                                        num_features),
-                                                       (preprocessor, cat_features),
-                                                       remainder = "passthrough")
-
-                encoder = preprocessor.transformers[1][1].__class__.__name__
-                pipeline = make_pipeline(preprocessor, model)
-                
-                result = scores(pipeline,
-                                X, y, 
-                                model_name, 
-                                model_params, 
-                                test = test, 
-                                encoder = encoder)
-
-                results.append(result)
-
-        if test == "feature_selection":
-            
-            preprocessor = make_column_transformer((SimpleImputer(strategy = "constant",
-                                                                  fill_value = -1,
-                                                                  add_indicator = True),
+    general_preprocessor = make_column_transformer((SimpleImputer(strategy = "constant",
+                                                                  fill_value = -1),
                                                     num_features),
                                                    (TargetEncoder(random_state = 42),
                                                     cat_features),
-                                                   remainder = "passthrough")
+                                                    remainder = "passthrough")
 
-            selection_thresholds = [0.001, 0.0015, 0.002, 0.005, 0.01, 0.02, 0.03]
-            
-            for threshold in selection_thresholds:
+    if models is not None:
+
+        results = []
+        params_to_save = ["class_weight", "min_samples_leaf", "n_estimators", "max_features"]
+        missings_handling_models = ["BaggingClassifier", "HistGradientBoostingClassifier"]
+    
+        for model in models:
+            model_name = model.__class__.__name__
+            model_params = ", ".join([f"{param}: {getattr(model, param)}"
+                                      for param in params_to_save
+                                      if hasattr(model, param)])
+    
+            if test == "imputation" and preprocessors is None:
+                if model_name in missings_handling_models:
+                    
+                    preprocessor = make_column_transformer((OneHotEncoder(), cat_features),
+                                                           remainder="passthrough")
+                    
+                    pipeline = make_pipeline(preprocessor, model)
+                    
+                    result = scores(pipeline,
+                                    X, y, 
+                                    model_name, 
+                                    model_params, 
+                                    test = test, 
+                                    imputation = "none")
+    
+                    results.append(result)
+    
+            if test == "imputation" and preprocessors is not None:
+                for imputer_name, preprocessor in preprocessors:
+                    
+                    preprocessor = make_column_transformer((preprocessor, num_features),
+                                                           (OneHotEncoder(), cat_features),
+                                                           remainder = "passthrough")
+    
+                    pipeline = make_pipeline(preprocessor, model)
+                    
+                    result = scores(pipeline,
+                                    X, y, 
+                                    model_name, 
+                                    model_params, 
+                                    test = test, 
+                                    imputation = imputer_name)
+    
+                    results.append(result)
+    
+            if test == "cat_encoding":
+                for preprocessor in preprocessors:
+                    
+                    preprocessor = make_column_transformer((SimpleImputer(strategy = "constant",
+                                                                          fill_value = -1),
+                                                            num_features),
+                                                           (preprocessor, cat_features),
+                                                           remainder = "passthrough")
+    
+                    encoder = preprocessor.transformers[1][1].__class__.__name__
+                    pipeline = make_pipeline(preprocessor, model)
+                    
+                    result = scores(pipeline,
+                                    X, y, 
+                                    model_name, 
+                                    model_params, 
+                                    test = test, 
+                                    encoder = encoder)
+    
+                    results.append(result)
+    
+            if test == "feature_selection":
+        
+                preprocessor = general_preprocessor
+
+                selection_thresholds = [0.005, 0.007, 0.01, 0.012, 0.014, 0.015, 0.017, 0.019, 0.024]
                 
-                pipeline = make_pipeline(
-                    preprocessor,
-                    SelectFromModel(estimator = 
-                                    RandomForestClassifier(class_weight = {1:50},
-                                                                       min_samples_leaf = 20,
-                                                                       random_state = 42,
-                                                                       n_jobs = -1),
-                                    threshold = threshold),
-                    model)
+                for threshold in selection_thresholds:
+                    
+                    pipeline = make_pipeline(
+                        preprocessor,
+                        SelectFromModel(estimator = RandomForestClassifier(n_estimators = 100,
+                                                                           class_weight = {1:75}, 
+                                                                           min_samples_leaf = 50,
+                                                                           max_features = None,
+                                                                           max_samples = 0.3,
+                                                                           random_state = 42, n_jobs = -1),
+                                        threshold = threshold),
+                        model)
+    
+                    result = scores(pipeline,
+                                    X, y,
+                                    model_name,
+                                    model_params,
+                                    test = test,
+                                    threshold = threshold)
+    
+                    results.append(result)
 
-                result = scores(pipeline,
-                                X, y,
-                                model_name,
-                                model_params,
-                                test = test,
-                                threshold = threshold)
+        return results
 
-                results.append(result)
-                
-    return results
+    if test == "searching_params":
+
+        if preprocessors is None:
+            preprocessor = general_preprocessor
+        else:
+            preprocessor = preprocessors
+        
+        model, param_grid = grid_search
+
+        pipeline = Pipeline([
+            ("preprocessor", preprocessor),
+            ("model", model)
+        ])
+
+        optimizer = GridSearchCV(pipeline, 
+                                 param_grid, 
+                                 cv = cv_scheme, 
+                                 scoring = "roc_auc", 
+                                 n_jobs = -1)
+        optimizer.fit(X, y)
+        best_params = optimizer.best_params_
+        best_params["best_auc_score"] = np.round(optimizer.best_score_, 4)
+        
+        return best_params
 
 
 def scores(pipeline, X, y, model_name, model_params, test, imputation = None, encoder = None, threshold = None):
@@ -153,17 +182,15 @@ def scores(pipeline, X, y, model_name, model_params, test, imputation = None, en
     Returns:
         dict: Dictionary containing test results.
     """
-
-    cv_scheme = StratifiedKFold(n_splits = 5, shuffle = True, random_state = 42)
     
     cv_results = cross_validate(pipeline, 
                                 X, y, 
-                                scoring = "roc_auc", 
+                                scoring = "roc_auc",
                                 cv = cv_scheme, 
                                 n_jobs = -1)
     
     roc_auc = np.round(cv_results["test_score"].mean(), 4)
-    time = np.round(cv_results["fit_time"].mean(), 2)
+    time = np.round((cv_results["fit_time"] + cv_results["score_time"]).mean(), 2)
 
     if test == "imputation":
         return {"Model": model_name,
@@ -172,7 +199,7 @@ def scores(pipeline, X, y, model_name, model_params, test, imputation = None, en
                 "Imputation": imputation,
                 "Time[s]": time}
 
-    if test == "encoders":
+    if test == "cat_encoding":
         return {"Model": model_name,
                 "Parameters": model_params,
                 "ROC_AUC": roc_auc,
@@ -205,16 +232,12 @@ def create_results_dataframe(*args):
         pd.DataFrame: A DataFrame containing the results with the model names as the index.
     """
     if len(args) == 1:
-        df = pd.DataFrame(args[0])
+        return pd.DataFrame(args[0])
     elif len(args) > 1:
-        df = pd.concat([pd.DataFrame(arg) for arg in args])
-
-    df.set_index("Model", inplace=True)
-
-    return df
+        return pd.concat([pd.DataFrame(arg) for arg in args])
 
 
-def display_results(dataframe, column):
+def summarize_results(dataframe, column_to_group_by):
     """
     Display summary statistics for a DataFrame grouped by a specified column.
 
@@ -235,7 +258,9 @@ def display_results(dataframe, column):
     pandas.io.formats.style.Styler
         A styled DataFrame with summary statistics and background gradient for specific columns.
     """
-    results = (dataframe.groupby(column)
+   
+    results = (dataframe
+               .groupby(column_to_group_by)
                .agg(
                    {"ROC_AUC": [np.size, np.mean, np.min, np.max],
                     "Time[s]": [np.mean, np.min, np.max]
@@ -245,5 +270,6 @@ def display_results(dataframe, column):
                .round(4))
 
     results[["mean_time[s]"]] = results[["mean_time[s]"]].round(2)
+    results = results.sort_values(by = ["max_roc_auc", "mean_roc_auc"], ascending = False)
 
     return results.style.background_gradient(subset = ["mean_roc_auc", "max_roc_auc", "mean_time[s]", "max_time[s]"])
